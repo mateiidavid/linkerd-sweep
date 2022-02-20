c0b66f3aa0e0b904dbb5ecbe49436e7bf3dd31a7
Matei David <matei@buoyant.io>
2022-02-20 14:05:08 +0000

    Deps
    Signed-off-by: Matei David <matei@buoyant.io>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::time::Duration;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    use std::env;
+
+    use tracing_subscriber::{prelude::*, EnvFilter};
+    let log_filter = env::var("RUST_LOG").unwrap_or_else(|_| String::from("info,kube-runtime=debug,kube=debug"))
+
+    Ok(())
 }
