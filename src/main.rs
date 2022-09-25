238562d9e1fb5dd4f78a17cad80b0f90734b6dd9
Matei David <matei@buoyant.io>
2022-09-25 15:16:06 +0000

    Add env var for log format
    Signed-off-by: Matei David <matei@buoyant.io>

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ struct Args {
     log_level: kubert::LogFilter,
 
     /// Log format (json | plain)
-    #[clap(long, default_value = "plain")]
+    #[clap(long, env = "LINKERD_SWEEP_LOG_FORMAT", default_value = "plain")]
     log_format: kubert::LogFormat,
 }
 
@@ -32,8 +32,8 @@ async fn main() -> Result<()> {
 
     let (_shutdown_tx, shutdown_rx) = kubert::shutdown::sigint_or_sigterm()?;
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 443));
-    let server = AdmissionServer::new(addr, shutdown_rx.clone());
+    let listen_addr = SocketAddr::from(([0, 0, 0, 0], 443));
+    let server = AdmissionServer::new(listen_addr, shutdown_rx.clone());
     let server_task = tokio::spawn(server.run());
     tokio::select! {
         _ = shutdown_rx.signaled() => {
